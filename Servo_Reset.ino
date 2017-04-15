1ea6973e9b6beaf3e66712757b9c1bf0746df442
cpprc <cpproboclub@gmail.com>
2017-04-15 16:09:23 -0700

    Add files via upload

---
+++ Servo_Reset.ino
@@ -0,0 +1,21 @@
+/* Cal Poly Pomona Robotics Club
+ * Robotic Arm Workshop
+ * https://github.com/CPPRoboticsClub/Robot_Arm
+ * Edited: April 15, 2017
+ */
+
+#include<Servo.h>
+Servo testServo; // Create Servo object
+void setup() {
+  testServo.attach(3); // Servo PWM Output Pin (Digital Pin ~3 on Arduino Uno)
+  pinMode(13,OUTPUT); // Arduino Built-In LED Pin
+}
+
+void loop() {
+  testServo.write(0); // Set Servo to 0 degree position
+  digitalWrite(13,1); // Turn test LED on
+  delay(3000); // 3 second delay
+  testServo.write(180); // Set Servo to 180 degree position
+  digitalWrite(13,0); // Turn test LED off
+  delay(3000); // 3 second delay
+}
